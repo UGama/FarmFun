60562c8f18191d9774c1a38edb68029c0d619abf
WM <wumianzw@126.com>
2019-04-13 14:29:24 +0800

    Update PcikActivity

--- app/src/main/java/com/gama/farm_fun/PickActivity.java
+++ app/src/main/java/com/gama/farm_fun/PickActivity.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,10 +18,12 @@ import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVFile;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.FindCallback;
 import com.avos.avoscloud.GetCallback;
 import com.avos.avoscloud.GetDataCallback;
 import com.avos.avoscloud.ProgressCallback;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -31,8 +34,12 @@ public class PickActivity extends AppCompatActivity {
     private TextView projectName;
     private TextView describe;
     private TextView address;
-    private TextView reserve;
+    private TextView ticket;
+    private TextView allTicketType;
     private RecyclerView ticketRecyclerView;
+    private List<Ticket> ticketList;
+
+    private ImageView loading;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,9 +69,15 @@ public class PickActivity extends AppCompatActivity {
         projectName = findViewById(R.id.projectName);
         describe = findViewById(R.id.describe);
         address = findViewById(R.id.address);
-        reserve = findViewById(R.id.reserve);
-        reserve.setText("门票预订");
+        ticket = findViewById(R.id.ticket);
+        ticket.setText("门票预订");
+        allTicketType = findViewById(R.id.allTicketType);
+        ticketList = new ArrayList<>();
         ticketRecyclerView = findViewById(R.id.ticketRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        ticketRecyclerView.setLayoutManager(linearLayoutManager);
+
+        loading = findViewById(R.id.loading);
 
         getProjectInformation();
     }
@@ -78,9 +91,47 @@ public class PickActivity extends AppCompatActivity {
                 describe.setText(object.getString("describe"));
                 address.setText(object.getString("locateDescribe"));
                 loadMainPic();
+                getTicket();
             }
         });
     };
+
+    public void getTicket() {
+        AVQuery<AVObject> ticketQuery = new AVQuery<>("Price");
+        ticketQuery.whereEqualTo("name",projectName.getText().toString());
+        ticketQuery.findInBackground(new FindCallback<AVObject>() {
+            @Override
+            public void done(List<AVObject> avObjects, AVException avException) {
+                allTicketType.setText("全部" + String.valueOf(avObjects.size()) + "种票型");
+                if (avObjects.size() <= 3) {
+                    for (int i = 0; i < avObjects.size(); i++) {
+                        AVObject avObject = avObjects.get(i);
+                        Ticket ticket = new Ticket(avObject.getString("name"), avObject.getString("ticketType"),
+                                avObject.getInt("price"), avObject.getInt("sales"));
+                        ticketList.add(ticket);
+                        Log.i("票型：", ticket.ticketType);
+                    }
+                } else {
+                    for (int i = 0; i < 3; i++) {
+                        AVObject avObject = avObjects.get(i);
+                        Ticket ticket = new Ticket(avObject.getString("name"), avObject.getString("ticketType"),
+                                avObject.getInt("price"), avObject.getInt("sales"));
+                        ticketList.add(ticket);
+                    }
+                }
+                Log.i("TicketList.size", String.valueOf(ticketList.size()));
+                ticketRecyclerView = findViewById(R.id.ticketRecyclerView);
+                LinearLayoutManager linearLayout = new LinearLayoutManager(PickActivity.this);
+                ticketRecyclerView.setLayoutManager(linearLayout);
+                TicketAdapter ticketAdapter = new TicketAdapter(ticketList);
+                ticketRecyclerView.setAdapter(ticketAdapter);
+                loading.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+    }
+
     public void loadMainPic(){
         AVQuery<AVObject> query = new AVQuery<>("_File");
         query.whereEqualTo("name", "pickmain.jpg");
@@ -116,8 +167,8 @@ public class PickActivity extends AppCompatActivity {
         @Override
         public TicketAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_recommendproject, parent, false);
-            final MainActivity.RecommendProjectsAdapter.ViewHolder holder = new MainActivity.RecommendProjectsAdapter.ViewHolder(view);
+                    .inflate(R.layout.item_ticket, parent, false);
+            final TicketAdapter.ViewHolder holder = new TicketAdapter.ViewHolder(view);
             /*holder.recommendProjectPic.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -128,25 +179,29 @@ public class PickActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(MainActivity.RecommendProjectsAdapter.ViewHolder holder, int position) {
-            Project project = projectsList.get(position);
-            Log.i(String.valueOf(position), project.title);
-            holder.recommendProjectText.setText(project.title);
+        public void onBindViewHolder(TicketAdapter.ViewHolder holder, int position) {
+            Ticket ticket = ticketList.get(position);
+            Log.i(String.valueOf(position), ticket.ticketType);
+            holder.ticketType.setText(ticket.ticketType);
+            holder.sales.setText(String.valueOf(ticket.sales));
+            holder.price.setText(String.valueOf(ticket.price));
         }
 
         @Override
         public int getItemCount() {
-            return projectsList.size();
+            return ticketList.size();
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder {
-            private ImageView recommendProjectPic;
-            private TextView recommendProjectText;
+            private TextView ticketType;
+            private TextView price;
+            private TextView sales;
 
             private ViewHolder(View view) {
                 super(view);
-                recommendProjectPic = view.findViewById(R.id.projectPic);
-                recommendProjectText = view.findViewById(R.id.projectText);
+                ticketType = view.findViewById(R.id.ticketType);
+                price = view.findViewById(R.id.price);
+                sales = view.findViewById(R.id.sales);
             }
         }
 
--- app/src/main/java/com/gama/farm_fun/Ticket.java
+++ app/src/main/java/com/gama/farm_fun/Ticket.java
@@ -5,10 +5,12 @@ public class Ticket {
     public int price;
     public String detail;
     public String ticketType;
+    public int sales;
 
-    public Ticket(String projectName, String ticketType, int price) {
+    public Ticket(String projectName, String ticketType, int price, int sales) {
         this.projectName = projectName;
         this.ticketType = ticketType;
         this.price = price;
+        this.sales = sales;
     }
 }
