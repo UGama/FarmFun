67133d61771210be34d96705c924b14e331a746c
WM <wumianzw@126.com>
2019-04-22 22:05:33 +0800

    Add Order

--- app/src/main/java/com/gama/farm_fun/OrderActivity.java
+++ app/src/main/java/com/gama/farm_fun/OrderActivity.java
@@ -5,8 +5,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class OrderActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
     }
 }
